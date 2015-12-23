86785117b3cf42c16cd82f2ed9ba0479ac44727c
Armin Braun <armin.b@icanlocalize.com>
2015-12-23 20:32:14 +0100

    phar skeleton start

---
+++ src/PhpTranspiler/Command/AnalyzeCommand.php
@@ -0,0 +1,19 @@
+<?php
+namespace PhpTranspiler\Command;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+class HelloCommand extends Command
+{
+	protected function configure()
+	{
+		$this
+			->setName('analyze')
+			->setDescription('Analyze code')
+			;
+	}
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$output->writeln('PHP Transpiler');
+	}
+
