0a2a714bf7e613aedad64020c5b4d1719f66941a
Armin Braun <armin.b@icanlocalize.com>
2015-12-24 19:46:58 +0100

    reformated Analyze Command

--- src/PhpTranspiler/Command/AnalyzeCommand.php
+++ src/PhpTranspiler/Command/AnalyzeCommand.php
@@ -1,24 +1,27 @@
 <?php
 namespace PhpTranspiler\Command;
+
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+
 class AnalyzeCommand extends Command
 {
-	protected function configure()
-	{
-		$this
-			->setName('analyze')
-			->setDescription('Analyze code')
-			->addArgument(
-				'path',
-				InputArgument::REQUIRED,
-				'What directory or script do you want to work on?'
-			);
-	}
-	protected function execute(InputInterface $input, OutputInterface $output)
-	{
-		$output->writeln('<info>PHP Transpiler</info>');
-	}
+    protected function configure()
+    {
+        $this
+            ->setName('analyze')
+            ->setDescription('Analyze code')
+            ->addArgument(
+                'path',
+                InputArgument::REQUIRED,
+                'What directory or script do you want to work on?'
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('<info>PHP Transpiler</info>');
+    }
 }
