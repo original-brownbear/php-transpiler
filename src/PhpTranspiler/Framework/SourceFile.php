1e547d1704b9dff71fd0905c4207e5208b318924
Armin Braun <armin.b@icanlocalize.com>
2015-12-25 17:49:38 +0100

    added functionality to coarsely identify PHP files

--- src/PhpTranspiler/Framework/SourceFile.php
+++ src/PhpTranspiler/Framework/SourceFile.php
@@ -9,10 +9,23 @@ class SourceFile
 
     public function __construct($path)
     {
-        if (false === is_dir($path)) {
+        if (false === is_file($path)) {
             throw new InvalidArgumentException('Given path does not contain a file.');
         }
 
         $this->url = $path;
+    }
+
+    public function isPhpFile()
+    {
+        $tokens = token_get_all(file_get_contents($this->url));
+        foreach ($tokens as $token) {
+            if ($token[0] === T_OPEN_TAG) {
+                $found_opening_tag = true;
+                break;
+            }
+        }
+
+        return ! empty($found_opening_tag);
     }
 }
--- tests/Framework/TestSourceFile.php
+++ tests/Framework/TestSourceFile.php
@@ -1,5 +1,6 @@
 <?php
 use \PhpTranspiler\Framework\SourceFile;
+use org\bovigo\vfs\vfsStream;
 
 class SourceFileTest extends \PHPUnit_Framework_TestCase
 {
@@ -9,5 +10,17 @@ class SourceFileTest extends \PHPUnit_Framework_TestCase
     public function testConstructorException()
     {
         new SourceFile('/fooo.php');
+    }
+
+    public function testIsPhpFile()
+    {
+        $source_path = '/src';
+        $vfs         = vfsStream::setup($source_path);
+        $file        = vfsStream::newFile('foo.php')->setContent('bar')->at($vfs);
+        $this->assertFalse((new SourceFile($file->url()))->isPhpFile());
+        $file = vfsStream::newFile('bar.php')->setContent('<?php')->at($vfs);
+        $this->assertFalse((new SourceFile($file->url()))->isPhpFile());
+        $file = vfsStream::newFile('bar.php')->setContent("<?php\necho 'test';")->at($vfs);
+        $this->assertTrue((new SourceFile($file->url()))->isPhpFile());
     }
 }
